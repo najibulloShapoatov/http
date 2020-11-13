630986096cac35192554b5d572e9d54ecc867a40
najibulloShapoatov <abduholiq007@gmail.com>
2020-11-13 09:34:05 +0500

    added comments

--- pkg/banners/service.go
+++ pkg/banners/service.go
@@ -17,7 +17,7 @@ func NewService() *Service {
 	return &Service{items: make([]*Banner, 0)}
 }
 
-//Banner ..
+//Banner ..Структура нашего баннера
 type Banner struct {
 	ID      int64
 	Title   string
@@ -26,7 +26,7 @@ type Banner struct {
 	Link    string
 }
 
-//это стартовый ID но для каждого создание поста его изменяем
+//это стартовый ID но для каждого создание баннера его изменяем
 var sID int64 = 0
 
 //All ...
@@ -42,7 +42,9 @@ func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	for _, v := range s.items {
+		//если ID элемента равно ID из параметра то мы нашли баннер
 		if v.ID == id {
+			//вернем баннер и ошибку nil
 			return v, nil
 		}
 	}
@@ -57,19 +59,22 @@ func (s *Service) Save(ctx context.Context, item *Banner) (*Banner, error) {
 
 	//Проверяем если id равно 0 то создаем баннер
 	if item.ID == 0 {
-		//увеличиваем стартовый индекс
+		//увеличиваем стартовый ID
 		sID++
-		//выставляем новый индекс для баннера
+		//выставляем новый ID для баннера
 		item.ID = sID
 		//добавляем его в слайс
 		s.items = append(s.items, item)
+		//вернем баннер и ошибку nil
 		return item, nil
 	}
-	//если нет то ишем его из сушествуеших
+	//если id не равно 0 то ишем его из сушествуеших
 	for k, v := range s.items {
 		//если нашли то заменяем старый баннер с новым
 		if v.ID == item.ID {
+			//если нашли то в слайс под индексом найденного выставим новый элемент
 			s.items[k] = item
+			//вернем баннер и ошибку nil
 			return item, nil
 		}
 	}
@@ -88,6 +93,7 @@ func (s *Service) RemoveByID(ctx context.Context, id int64) (*Banner, error) {
 		if v.ID == id {
 			//берем все элементы до найденного и добавляем в него все элементы после найденного
 			s.items = append(s.items[:k], s.items[k+1:]...)
+			//вернем баннер и ошибку nil
 			return v, nil
 		}
 	}
